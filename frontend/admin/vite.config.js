5b0fc8d165cb55c9e1a3a91acd280d967fef1734
attajnr2001 <attajnr731@gmail.com>
2024-08-09 04:28:57 +0000

    updates

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/P
 export default defineConfig(({ mode }) => {
@@ -12,7 +13,17 @@ export default defineConfig(({ mode }) => {
     optimizeDeps: {
       exclude: ["@reduxjs/toolkit/query/react"],
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      visualizer({
+        open: true,
+        filename: "dist/stats.html",
+      }),
+    ],
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
+
     server: {
       proxy: {
         "/api": {
