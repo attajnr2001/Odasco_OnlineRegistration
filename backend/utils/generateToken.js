a9e8e8ee12de8d66e99189ce31b609cde3be1e9b
attajnr2001 <attajnr731@gmail.com>
2024-08-09 10:51:02 +0000

    token period changed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "7d", // Changed from 30d to 7d
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
     sameSite: "strict", // Prevent CSRF attacks
-    maxAge: 0.5 * 24 * 60 * 60 * 1000, // 30 days
+    maxAge: 8 * 60 * 60 * 1000, // 8 hours (changed from 12 hours)
   });
 };
 
