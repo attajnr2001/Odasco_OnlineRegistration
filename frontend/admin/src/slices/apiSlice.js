314121f35a6f0dce9a98323c6a9ef8e8dd0576d6
attajnr2001 <attajnr731@gmail.com>
2024-08-09 04:57:13 +0000

    import.meta.env -> process.env

--- frontend/admin/src/slices/apiSlice.js
+++ frontend/admin/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = import.meta.env.VITE_API_URL || "/api";
+const baseUrl = process.env.VITE_API_URL || "/api";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
