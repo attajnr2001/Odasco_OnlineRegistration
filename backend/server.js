0390e1ffda7bd0b172cf751b28a73fa90b962430
attajnr2001 <attajnr731@gmail.com>
2024-08-09 03:51:25 +0000

    updates

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import studentRoutes from "./routes/studentRoutes.js";
 import clientRoutes from "./routes/clientRoutes.js";
 import logRoutes from "./routes/logRoutes.js";
 import pdfRoutes from "./routes/pdfRoutes.js";
+import cors from "cors";
 
 const port = process.env.PORT || 5000;
 connectDB();
@@ -19,6 +20,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: "http://localhost:3000",
+          target: "https://odasco-onlineregistration.onrender.com",
           changeOrigin: true,
         },
       },
